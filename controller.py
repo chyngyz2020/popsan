7116b9627dee9356150354cc63829c671ca497e6
chyngyz <chyngyzbatyrov170@gmail.com>
2023-11-12 20:42:53 +0600

    ana saga

---
+++ controller.py
@@ -0,0 +1,12 @@
+
+from moduls.model.model import (
+     save_data,
+     append_data,
+)
+cle
+def save_data_controller(f_name, data):
+    save_data(f_name, data)
+
+def append_data_controller(f_name, data):
+    append_data(f_name, data)
+
